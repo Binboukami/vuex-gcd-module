c8a21b1c70188706c2403c362643fbaecd604462
Luiz Alex Butkeivicz <luisalexsh@hotmail.com>
2021-12-01 11:12:05 -0300

    renamed local variable 'event' to 'current_event'

--- modules/gcd/mutations.js
+++ modules/gcd/mutations.js
@@ -4,26 +4,26 @@ export default {
   },
   processTime(state, payload) {
     //local variables
-    const event = state.gcd.current_event;
+    const current_event = state.gcd.current_event;
     const config = state.gcd.configuration;
     const fdate = state.gcd.current_event.display_date;
 
     //calculates remaining time between actual and expiration date
-    event.remaining_time = payload.event_date - payload.current_date;
+    current_event.remaining_time = payload.event_date - payload.current_date;
 
     //main function loop
-    if (event.remaining_time > 0) {
-      event.status = true;
+    if (current_event.remaining_time > 0) {
+      current_event.status = true;
       let tx = 24;
       const udate = Object.keys(fdate);
 
-      //Formats time and store values in event.formated_date{}
+      //Formats time and store values in current_event.formated_date{}
       udate.forEach((t) => {
         if (t == "min" || t == "sec") {
           tx = 60;
         }
         fdate[t] = Math.floor(
-          (event.remaining_time / config.definitions.date[t]) % tx
+          (current_event.remaining_time / config.definitions.date[t]) % tx
         ).toString();
         if (fdate[t].length < 2) {
           fdate[t] = "0" + fdate[t];
@@ -32,7 +32,7 @@ export default {
 
       //Your custom code here
     } else {
-      event.status = false;
+      current_event.status = false;
       clearInterval(state.gcd.configuration.interval.ref);
     }
   },
