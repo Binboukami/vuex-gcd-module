8a7cd4ae0ccf9f8e533c25d5c3ef537f19884326
Luiz Alex Butkeivicz <luisalexsh@hotmail.com>
2021-12-01 07:12:25 -0300

    refactored getters

--- modules/gcd/getters.js
+++ modules/gcd/getters.js
@@ -1,11 +1,20 @@
 export default {
-	getTime: (state) => {
-		return state.globalCountdown.event;
-	},
-	getOptions: (state) => {
-		return state.globalCountdown.options;
-	},
-	getFormatedTime: (state) => {
-		return state.globalCountdown.event.formated_date;
-	},
+  $gcd: (state) => {
+    return state.gcd;
+  },
+  $currentEvent: (state) => {
+    return state.gcd.current_event;
+  },
+  $user: (state) => {
+    return state.gcd.user;
+  },
+  $events: (state) => {
+    return state.gcd.events;
+  },
+  $config: (state) => {
+    return state.gcd.configuration;
+  },
+  $displayDate: (state) => {
+    return state.gcd.currentEvent.display_date;
+  },
 };
