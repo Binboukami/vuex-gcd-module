b26b0924a085b43d764a024563ba1fa72cfe8686
Luiz Alex Butkeivicz <luisalexsh@hotmail.com>
2021-12-27 17:37:38 -0300

    implemented default values for 'events' array and objects

--- modules/gcd/state.js
+++ modules/gcd/state.js
@@ -18,34 +18,24 @@ export default {
 				sec: null,
 			},
 		},
-        id: "event_A",
-        description: "Event_A happens when x conditions are met",
-        date: null,
-      },
-      {
-        id: "event_B",
-        description: "Event_B happens when y conditions are met",
-        date: null,
-      },
-    ],
-    configuration: {
-      interval: {
-        ref: null,
-        update: 1000,
-      },
-      properties: {
-        //Things like date formating in DD/MM/YYYY or MM/DD/YYYYY
-        //And also stuff related to user/dev preferences
-      },
-      definitions: {
-        date: {
-          weeks: 1000 * 60 * 60 * 24 * 7,
-          days: 1000 * 60 * 60 * 24,
-          hours: 1000 * 60 * 60,
-          min: 1000 * 60,
-          sec: 1000,
-        },
-      },
-    },
-  },
+		events: [
+			{
+				id: "event_Zero",
+				description: "event_Zero happens when initial conditions are met",
+				event_date: new Date(Date.UTC(2021, 11, 28, 21, 0)),
+				expiration_date: new Date(Date.UTC(2021, 11, 2, 22, 0))
+			},
+			{
+				id: "event_A",
+				description: "Event_A happens when x conditions are met",
+				event_date: new Date(Date.UTC(2021, 11, 28, 12, 0)),
+				expiration_date: new Date(Date.UTC(2021, 11, 2, 22, 0)),
+			},
+			{
+				id: "event_B",
+				description: "Event_B happens when y conditions are met",
+				event_date: new Date(Date.UTC(2020, 2, 1, 22, 0)),
+				expiration_date: new Date(Date.UTC(2021, 10, 30, 22, 0)),
+			},
+		],
 };
