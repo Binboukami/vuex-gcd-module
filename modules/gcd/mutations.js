9c92d4c823f50f228778a77b216c6a283fb82aeb
Luiz Alex Butkeivicz <luisalexsh@hotmail.com>
2021-12-27 17:44:40 -0300

    refactored functions and implemented 'setDisplayTime' mutation

--- modules/gcd/mutations.js
+++ modules/gcd/mutations.js
@@ -13,35 +13,22 @@ export default {
 			clearInterval(state.gcd.configuration.interval.ref);
 		}
 	},
-    const config = state.gcd.configuration;
-    const fdate = state.gcd.current_event.display_date;
+	setDisplayTime(state) {
+		const current_event = state.gcd.current_event;
+		const display_date = state.gcd.current_event.display_date;
+		const config = state.gcd.configuration;
 
-    //calculates remaining time between actual and expiration date
-    current_event.remaining_time = payload.event_date - payload.current_date;
+		const arrDisplayDate = Object.keys(display_date);
+		let tx = 24;
 
-    //main function loop
-    if (current_event.remaining_time > 0) {
-      current_event.status = true;
-      let tx = 24;
-      const udate = Object.keys(fdate);
-
-      //Formats time and store values in current_event.formated_date{}
-      udate.forEach((t) => {
-        if (t == "min" || t == "sec") {
-          tx = 60;
-        }
-        fdate[t] = Math.floor(
-          (current_event.remaining_time / config.definitions.date[t]) % tx
-        ).toString();
-        if (fdate[t].length < 2) {
-          fdate[t] = "0" + fdate[t];
-        }
-      });
-
-      //Your custom code here
-    } else {
-      current_event.status = false;
-      clearInterval(state.gcd.configuration.interval.ref);
-    }
-  },
+		arrDisplayDate.forEach((t) => {
+			if (t == "min" || t == "sec") {
+				tx = 60;
+			}
+			display_date[t] = Math.floor((current_event.remaining_time / config.definitions.date[t]) % tx).toString();
+			if (display_date[t].length < 2) {
+				display_date[t] = "0" + display_date[t];
+			}
+		});
+	},
 };
